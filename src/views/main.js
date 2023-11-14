3becb693add334e4da4a46bd289aeb3700019b70
SamserAliKhan <samseralikhan0448@gmail.com>
2023-11-14 18:25:21 +0530

    deleted:    src/views/heading.js 	modified:   src/views/index.html 	modified:   src/views/main.js

--- src/views/main.js
+++ src/views/main.js
@@ -1,41 +1,47 @@
 // Smooth scrolling when clicking on links with hash tags
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
+  anchor.addEventListener('click', function (e) {
       e.preventDefault();
-  
+
       const targetId = this.getAttribute('href').substring(1);
       const targetElement = document.getElementById(targetId);
-  
-      if (targetElement) {
-        window.scrollTo({
-          top: targetElement.offsetTop - 50, // Adjust as needed
-          behavior: 'smooth'
-        });
-      }
-    });
-  });
-  const menuBar = document.querySelector('.fa-bars');
-        const menuItems = document.querySelector('ul');
 
-        menuBar.addEventListener('click', () => {
-            menuItems.classList.toggle('hidden');
-        });
-        document.addEventListener('DOMContentLoaded', function () {
-          const textElement = document.getElementById('text');
-          const text = "Hello! I'm [Your Name]. Welcome to my portfolio.";
-      
-          function typeWriter(text, i, callback) {
-              if (i < text.length) {
-                  textElement.innerHTML += text.charAt(i);
-                  i++;
-                  setTimeout(function () {
-                      typeWriter(text, i, callback);
-                  }, 50); // Adjust the typing speed (milliseconds)
-              } else if (typeof callback === 'function') {
-                  setTimeout(callback, 1000); // Adjust the pause before the cursor blinks again
-              }
-          }
-      
-          typeWriter(text, 0);
-      });
-      
+      if (targetElement) {
+          window.scrollTo({
+              top: targetElement.offsetTop - 50,
+              behavior: 'smooth'
+          });
+      }
+  });
+});
+
+// Check if the elements exist before adding event listeners
+const menuBar = document.querySelector('#sideNav'); // Assuming 'sideNav' is an ID
+const menuItems = document.querySelector('ul');
+
+if (menuBar) {
+  menuBar.addEventListener('click', () => {
+      if (menuItems) {
+          menuItems.classList.toggle('hidden');
+      }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const textElement = document.getElementById('text');
+  const text = "Hello! I'm Samser Ali Khan. Welcome to my portfolio!";
+  textElement.style.fontSize = '24px';
+  function typeWriter(text, i, callback) {
+      if (i < text.length) {
+          textElement.innerHTML += text.charAt(i);
+          i++;
+          setTimeout(function () {
+              typeWriter(text, i, callback);
+          }, 50);
+      } else if (typeof callback === 'function') {
+          setTimeout(callback, 1000);
+      }
+  }
+
+  typeWriter(text, 0);
+});
